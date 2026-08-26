bf49013cb01c9aba49b9f718553b4381bdb27283
simplysaadali <ali091g@gmail.com>
2026-08-26 16:27:57 +0500

    Auth middleware and Message Schema change & add

---
+++ server/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+function protect(req, res, next) {
+  const token = req.cookies.token;
+  if (!token)
+    return res.status(401).json({ message: "Unauthorised — No token" });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { 
+        id: decoded.id,
+        role: decoded.role
+    };
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token invalid or expired" });
+  }
+}
+
+module.exports = protect;
--- server/models/Message.js
+++ server/models/Message.js
@@ -1,23 +1,45 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: true,
-        trim: true,
-        maxlength: 1000,
+        text: {
+            type: String,
+            required: true,
+            trim: true,
+            maxlength: 1000,
+        },
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User", //User means User model, tells mongoDB what user is used here, in the ID
+            required: true,
+            index: true
+        },
+        recipient: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            index: true
+        },
+        authorType: {
+            type: String,
+            enum: ["user", "bot"],
+            default: "user",
+            index: true,
+        },
     },
-    sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-        index: true
-    },
-    recipient: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-        index: true
-    },
-    
-})
+    { timestamps: true },
+);
+
+//static means you're creating your own in schema model
+messageSchema.statics.between = function (a, b) { 
+  return this.find({
+     //$or gives both ways send and receive, without this, it is not possible to send and receive from both sides
+    $or: [
+      { sender: a, recipient: b },
+      { sender: b, recipient: a },
+    ],
+  })
+    .sort({ createdAt: 1 }) //newest first
+    .limit(150); //maximum nmber of messages to load
+};
+
+module.exports = mongoose.model("Message", messageSchema);
