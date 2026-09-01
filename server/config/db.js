037b464634fe527a95abef2faa1d8f482b74cc42
simplysaadali <ali091g@gmail.com>
2026-09-01 22:29:25 +0500

    updation to avoid errors, error handling

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 
+const url = process.env.DB_URL;
+
 async function connectDB (){
-    await mongoose.connect(process.env.DB_URL);
-    console.log("Connected to the DB successfully");
+    try {
+        await mongoose.connect(url);
+        console.log("Connected to the DB successfully");
+    } catch (error) {
+        console.error("Error connecting to the Data Base: ", error);
+    }
 }
 
 export default connectDB;
